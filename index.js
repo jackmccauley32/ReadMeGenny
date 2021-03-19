30d96f8b83fe3d0211ce1727adb60d216eb2aaa8
John McCauley <jackmccauley32@gmail.com>
2021-03-19 14:51:13 -0500

    licenselist

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require("fs")
+const fs = require("fs");
+const Choices = require("inquirer/lib/objects/choices");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -45,9 +46,10 @@ const questions = [
     name: '',
 },
 {
-    type: 'input',
-    message: '',
-    name: '',
+    type: 'list',
+    message: 'If applicable, please select a license below',
+    name: 'license',
+    Choices: ['MIT', 'Apache', 'BSD', 'none']
 },
 ];
 
